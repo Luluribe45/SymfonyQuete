2861e5695b995b3d275b7853d9576564093fa49b
Lucile Varloteaux <varloteaux.lucile@gmail.com>
2018-11-16 15:00:56 +0100

    création formulaire

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -8,9 +8,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Entity\Category;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\CategoryType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
@@ -22,6 +25,38 @@ class CategoryController extends AbstractController
     {
         return $this->render('category/showCategory.html.twig', [
             'category'=>$category
+            ]
+        );
+    }
+    /**
+     * Form add a category
+     *
+     * @Route("/category",
+     *     name = "blog_add_category")
+     * @param $request Request
+     * @return Response A response instance
+     *
+     */
+    public function addFormCategory(Request $request) : Response
+    {
+        // 1. Build the form
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+
+        // 2. handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // 3. Save the category
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('blog_list_categories');
+        }
+
+        return $this->render(
+            'blog/addCategory.html.twig', [
+                'form' => $form->createView(),
             ]
         );
     }
