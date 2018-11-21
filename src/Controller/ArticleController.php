404bf67d51fa3f2ebf1cc24327498881cf851850
Lucile Varloteaux <varloteaux.lucile@gmail.com>
2018-11-21 10:02:19 +0100

    form2

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/article")
+ */
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/", name="article_index", methods="GET")
+     */
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('article/index.html.twig', ['articles' => $articleRepository->findAll()]);
+    }
+}
